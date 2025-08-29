a9be640341d25e8d9f3c84002f45ab92f5f66263
theg0607 <akashrajput2363@gmail.com>
2025-08-30 04:32:40 +0530

    trying checkout on my second commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,12 @@
 "use client"
-<<<<<<< HEAD
-=======
 import Signup from "@/pages/Signup";
 import login from "@/pages/login";
->>>>>>> fca58c875253833e624677849ea540b7a8b35db1
 import Image from "next/image";
-import Signup from "./signup/page";
-import Login from "./login/page";
 
 export default function Home() {
   return (
     <div>
-<<<<<<< HEAD
-    <Signup/>
-=======
      <login/>
->>>>>>> fca58c875253833e624677849ea540b7a8b35db1
     </div>
   );
 }
